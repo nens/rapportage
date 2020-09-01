7f0e762544faab9e392e73811d21729a33996d32
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2020-09-01 18:13:25 +0200

    attempt rain report proxy

--- app/components/api/api.js
+++ app/components/api/api.js
@@ -13,18 +13,30 @@ angular.module('api', [])
     'window=1200000';
 
   var redirect = function () {
-    // Comment out the next 2 lines to prevent the redirect for development.
     window.location.href = '//' + window.location.host +
         '/accounts/login/?next=' + window.location.href;
   };
 
+  var replaceUuid = function (preUrl, uuid) {
+    // replace uuid with staging uuid if on staging
+    var url = preUrl;
+    if ((window.location.href+'').indexOf('staging') !== -1) {
+      url = preUrl.replace(/\{uuid\}/g, "3e5f56a7-b16e-4deb-8449-cc2c88805159");
+    } else {
+      url = preUrl.replace(/\{uuid\}/g, uuid);
+    }
+    return url;
+  }
+
   var rainRecurrence = function (location, uuid, date) {
-    var url = RRC_BASE_URL
+    var preUrl = RRC_BASE_URL
     .replace('{lng}', location.geometry.lng)
     .replace('{lat}', location.geometry.lat)
     .replace('{start_date}', date.start)
-    .replace('{stop_date}', date.stop)
-    .replace(/\{uuid\}/g, uuid);
+    .replace('{stop_date}', date.stop);
+
+    var url = replaceUuid(preUrl, uuid);
+
     return $http.get(url, {withCredentials: true}).then(function (response) {
       if (response.data.data === null) {
         redirect();
@@ -47,19 +59,25 @@ angular.module('api', [])
   + 'stop={stop}-12-31T23:00:00&'
   + 'window=2635200000';
 
+
+
   var getMonthlyRain = function (bounds, year, uuid) {
     var north = bounds._northEast.lat;
     var east = bounds._northEast.lng;
     var south = bounds._southWest.lat;
     var west = bounds._southWest.lng;
-    var url = RAIN_BASE_URL
+    var preUrl = RAIN_BASE_URL
     .replace('{start}', year - 1) // year starts a few hours before our year -- timezones
     .replace('{stop}', year) // year starts a few hours before our year -- timezones
     .replace(/\{north\}/g, north)
     .replace(/\{south\}/g, south)
     .replace(/\{east\}/g, east)
-    .replace(/\{west\}/g, west)
-    .replace(/\{uuid\}/g, uuid);
+    .replace(/\{west\}/g, west);
+
+    var url = replaceUuid(preUrl, uuid);
+
+    console.log('url', url);
+
     return $http.get(url, {withCredentials: true}).then(function (response) {
       if (response.data.data === null) {
         redirect();
